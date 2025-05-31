4fa3eac1181a882b3b4aa9adb0209acfe5a616af
joaorocha-web <jvrochajf@eaportal.org>
2025-05-31 14:36:14 -0300

    .

--- resources/views/teste/teste.blade.php
+++ resources/views/teste/teste.blade.php
@@ -28,51 +28,154 @@
     <script>
         const screen = document.getElementById('screen')
         const ctx = screen.getContext('2d')
+        const currentPlayerId = 'player1'
 
-        
+        function createGame(){
+            const state = {
+                players: {},
+                fruits: {} 
+            }
 
-        const game = {
-            players: {
-                'player1': {x:1, y:2},
-                'player2' : {x:4, y:4}
-            },
+            function addPlayer(command){
+                const playerId = command.playerId
+                const playerX = command.playerX
+                const playerY = command.playerY
 
-            fruits: {
-                'fruit1' : {x:2, y:3}
+                state.players[playerId] = {
+                    x: playerX,
+                    y: playerY
+                }
+            }
+
+            function removePlayer(command){
+                const playerId = command.playerId
+                
+                delete state.players[playerId]
+            }
+
+            function addFruit(command){
+                const fruitId = command.fruitId
+                const fruitX = command.fruitX
+                const fruitY = command.fruitY
+
+                state.fruits[fruitId] = {
+                    x: fruitX,
+                    y: fruitY
+                }
+            }
+
+            function removeFruit(command){
+                const fruitId = command.fruitId
+
+                delete state.fruits[fruitId]
+            }
+
+            function movePlayer(command){
+                console.log(`Moving ${command.playerId} with ${command.keyPressed}`)
+
+                const acceptedMoves = {
+                    ArrowUp(player) {
+                        console.log(`Moving player up`)
+                        if(player.y - 1 >=0){
+                            player.y = player.y - 1
+                        }       
+                    },
+                    ArrowDown(player) {
+                        console.log(`Moving player down`)
+                        if(player.y + 1 <=9){
+                            player.y = player.y + 1
+                        }
+                    },
+                    ArrowLeft(player) {
+                        console.log(`Moving player left`)
+                        if(player.x - 1 >=0){
+                             player.x = player.x - 1
+                        }
+                    },
+                    ArrowRight(player) {
+                        console.log(`Moving player right`)
+                        if(player.x + 1 <=9){
+                            player.x = player.x + 1
+                        }
+                    }
+                }
+
+                const keyPressed = command.keyPressed
+                const player = state.players[command.playerId]
+                const moveFunction = acceptedMoves[keyPressed]
+                
+                if(moveFunction){
+                    moveFunction(player)
+                }
+                return
+
+            }
+
+            return {
+                addFruit, 
+                removeFruit,
+                addPlayer,
+                removePlayer,
+                movePlayer,
+                state
             }
         }
 
-        document.addEventListener('keydown', handleKeydown)
+        const game = createGame()
+        const keyboardListener = createKeyboardListener()
+        keyboardListener.subscribe(game.movePlayer)
 
-        function handleKeydown(event){
-            const keyPressed = event.key
-            const player = game.players['player1']
-            if(keyPressed === 'ArrowUp' && player.y - 1 >=0){
-                player.y = player.y - 1
+        function createKeyboardListener(){
+            const state = {
+                observers: []
             }
-            if(keyPressed === 'ArrowDown' && player.y + 1 <=9){
-                player.y = player.y + 1
+
+            function subscribe(observerFunction){
+                state.observers.push(observerFunction)
             }
-            if(keyPressed === 'ArrowLeft' && player.x - 1 >=0){
-                player.x = player.x - 1
+
+            function notifyAll(command){
+                console.log(`Notifying ${state.observers.length} observers`)
+
+                for(const observerFunction of state.observers){
+                    observerFunction(command)
+                }
             }
-            if(keyPressed === 'ArrowRight' && player.x + 1 <=9){
-                player.x = player.x + 1
+
+            document.addEventListener('keydown', handleKeydown)
+
+            function handleKeydown(event){
+                const keyPressed = event.key
+
+                const command = {
+                    playerId: 'player1',
+                    keyPressed
+                }
+
+                notifyAll(command)
+                
+            }
+
+            return{
+                subscribe
             }
         }
+
+
+       
 
         renderGame()
 
         function renderGame(){
             ctx.fillStyle = 'white'
             ctx.clearRect(0,0, 10, 10)
-            for(const playerId in game.players){
-                const player = game.players[playerId]
+            for(const playerId in game.state.players){
+                const player = game.state.players[playerId]
                 ctx.fillStyle = 'black'
                 ctx.fillRect(player.x, player.y, 1 ,1)
             }
-            for(const fruitId in game.fruits){
-                const fruit = game.fruits[fruitId]
+            for(const fruitId in game.state.fruits){
+                const fruit = game.state.fruits[fruitId]
                 ctx.fillStyle = 'green'
                 ctx.fillRect(fruit.x, fruit.y, 1,1)
             }
